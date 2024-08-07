5335c1e8ad4b9aa04fc94180e7a1c9d1593163ae
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-08-07 03:22:58 +0200

    added python backend and also now showing the dungeons pictures on the screen depending on the input a user is giving

--- src/app/components/MythicPlusForm.tsx
+++ src/app/components/MythicPlusForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 type Props = {};
@@ -8,21 +8,39 @@ function MythicPlusForm({}: Props) {
   const [dungeonName, setDungeonName] = useState("");
   const [guide, setGuide] = useState("");
   const [classes, setClasses] = useState("");
+  const [src, setSrc] = useState("");
+
+  useEffect(() => {
+    if (dungeonName.includes("Ara-Kara")) {
+      setSrc("/Ara-Kara.png");
+    } else {
+      setSrc("");
+    }
+  }, [dungeonName]);
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const response = await fetch(
-      "http://127.0.0.1:5000/get_mythic_plus_guide",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ dungeon_name: dungeonName, classes: classes }),
-      },
-    );
 
-    const data = await response.json();
-    setGuide(data.guide);
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:5000/get_mythic_plus_guide",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ dungeon_name: dungeonName, classes: classes }),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      setGuide(data.guide);
+    } catch (error) {
+      console.error("There was an error:", error);
+    }
   };
 
   return (
@@ -32,6 +50,7 @@ function MythicPlusForm({}: Props) {
           <label>Dungeon Name:</label>
           <input
             type="text"
+            className="text-black"
             placeholder="Which Dungeon are you running?"
             value={dungeonName}
             onChange={(e) => setDungeonName(e.target.value)}
@@ -41,6 +60,7 @@ function MythicPlusForm({}: Props) {
           <label>Affixes:</label>
           <input
             type="text"
+            className="text-black"
             placeholder="Which Classes are in your group?"
             value={classes}
             onChange={(e) => setClasses(e.target.value)}
@@ -48,9 +68,11 @@ function MythicPlusForm({}: Props) {
         </div>
         <button type="submit">Get Guide</button>
       </form>
+      {dungeonName ? <img src={src} alt="" /> : null}
       {guide && (
         <div>
           <h3>Guide:</h3>
+          {dungeonName ? <h4>{dungeonName}</h4> : null}
           <p>{guide}</p>
         </div>
       )}
