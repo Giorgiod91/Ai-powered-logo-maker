7b5b8c175397c6a7a0b4b42abe111678259e5cb0
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-08-12 20:37:02 +0200

    bug fix

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import fetch from "node-fetch";
+
 interface ApiResponse {
   image_url?: string;
 }
@@ -7,10 +8,17 @@ interface ApiResponse {
 export async function POST(request: Request) {
   const API_URL = process.env.API_URL;
 
+  if (!API_URL) {
+    return NextResponse.json(
+      { error: "API_URL is not defined in environment variables" },
+      { status: 500 },
+    );
+  }
+
   try {
     const body = await request.json();
 
-    const response = await fetch(API_URL!, {
+    const response = await fetch(API_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -18,7 +26,11 @@ export async function POST(request: Request) {
       body: JSON.stringify(body),
     });
 
-    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    const data: ApiResponse = (await response.json()) as ApiResponse;
     return NextResponse.json(data, { status: response.status });
   } catch (error) {
     return NextResponse.json(
